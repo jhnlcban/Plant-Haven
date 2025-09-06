fc574eb7eced165b591fba67e447d4686bac5e15
John Paulo <167491499+jhnlcban@users.noreply.github.com>
2025-09-06 10:44:02 +0800

    Add files via upload

---
+++ includes/product-data.php
@@ -0,0 +1,40 @@
+<?php
+
+$products = [
+  [
+    "id" => 1,
+    "name" => "Succulent Starter Pack",
+    "price" => 499.00,
+    "image" => "product-images/Succulent Starter Pack.jpg"
+  ],
+  [
+    "id" => 2,
+    "name" => "Mini Bonsai Tree",
+    "price" => 899.00,
+    "image" => "product-images/Mini Bonsai Tree.jpg"
+  ],
+  [
+    "id" => 3,
+    "name" => "Indoor Snake Plant",
+    "price" => 750.00,
+    "image" => "product-images/Indoor Snake Plant.jpg"
+  ],
+  [
+    "id" => 4,
+    "name" => "Herb Garden Kit (Basil, Mint, Oregano)",
+    "price" => 599.00,
+    "image" => "product-images/Herb Garden Kit (Basil, Mint, Oregano).jpg"
+  ],
+  [
+    "id" => 5,
+    "name" => "Flowering Peace Lily",
+    "price" => 680.00,
+    "image" => "product-images/Flowering Peace Lily.jpg"
+  ],
+  [
+    "id" => 6,
+    "name" => "Hanging Fern Basket",
+    "price" => 425.00,
+    "image" => "product-images/Hanging Fern Basket.jpg"
+  ]
+];
